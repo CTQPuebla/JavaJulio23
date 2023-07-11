13438f8c4e598d5453a5e729e3db0d5425bc472b
Felipes3048 <felipin3078@gmail.com>
2023-07-11 13:40:26 -0600

    Practica de objetos

---
+++ herencia_fesr/src/com/Empleado.java
@@ -0,0 +1,61 @@
+package com;
+
+public class Empleado extends Persona {
+	String rfc;
+	String nss;
+	double salario;
+	String horario;
+
+	public Empleado() {
+
+	}
+
+	public Empleado(String nombre, String sexo, String peso, String altura, String rfc, String nss, double salario) {
+		super(nombre, sexo, peso, altura);
+		this.rfc = rfc;
+		this.nss = nss;
+		this.salario = salario;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [rfc=" + rfc + ", nss=" + nss + ", salario=" + salario + ", horario=" + horario + "]";
+	}
+
+	public String getRfc() {
+		return rfc;
+	}
+
+	public void setRfc(String rfc) {
+		this.rfc = rfc;
+	}
+
+	public String getNss() {
+		return nss;
+	}
+
+	public void setNss(String nss) {
+		this.nss = nss;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public void setHorario(String horario) {
+		this.horario = horario;
+	}
+	public void otroMetodo() {
+		System.out.println("hasta que no vean carne coman normal");
+	}
+	
+	
+}
