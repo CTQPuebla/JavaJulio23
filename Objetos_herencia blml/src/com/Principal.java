7a4c90b4e198e8d3d9049f79adf296eb91f24dd4
banyys <bany.9@msn.com>
2023-07-11 13:39:33 -0600

    practica de objetos

---
+++ Objetos_herencia blml/src/com/Principal.java
@@ -0,0 +1,16 @@
+package com;
+
+public class Principal {
+public static void main(String[] args) {
+	
+	Persona p= new Persona("Laura", "Femenino", "Setenta kilos", "1.65");
+	
+	
+	Empleado e= new Empleado ();
+	
+	
+}
+	
+	
+	
+}
