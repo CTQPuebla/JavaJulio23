171a940cf328c79ac1c0c78c7e595a518e06de29
cristhianhernandez96 <chriz.aries_1996@hotmail.com>
2023-07-11 13:30:48 -0600

    practica de herencia

---
+++ objetos-herencia-CJHM/src/com/Principal.java
@@ -0,0 +1,18 @@
+package com;
+
+public class Principal {
+	public static void main(String[] args) {
+     
+		
+		
+		Persona p=new Persona("Cristhian", "Masculino", "70.0 kg", "1.73");
+		
+		
+		
+		p.setPeso("85");
+		System.out.println(p);
+		
+		Empleado e=new Empleado();
+		
+	}
+}
