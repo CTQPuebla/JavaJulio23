dd45cfa3deae1ba320e8738d4062d90dcd9cef11
Maleny2 <maleni.corte93@gmail.com>
2023-07-12 10:49:11 -0600

    practica

---
+++ Objetos_herencia JMC/src/com/Principal.java
@@ -0,0 +1,17 @@
+package com;
+
+public class Principal{
+	public static void main(String[] args) {
+		
+		Persona p=new Persona("Laura","Femenino","70.0kg","1.65");	
+	p.setPeso("75.0");
+	System.out.println(p);
+	Empleado e=new Empleado();
+	
+	
+	}
+
+	
+	
+}
+
