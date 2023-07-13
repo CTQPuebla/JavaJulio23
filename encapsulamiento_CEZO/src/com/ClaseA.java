ed510927708c2ba23f6e5fc46f08fc63998a404a
Emmanuel <caaysoem@gmail.com>
2023-07-13 08:59:51 -0600

    Abstracta y encapsulamiento

---
+++ encapsulamiento_CEZO/src/com/ClaseA.java
@@ -0,0 +1,59 @@
+package com;
+
+import java.sql.Date;
+
+public class ClaseA {
+
+	public String atributoA;
+	protected int atributoB;
+	private double atributoC;
+	protected Date fecha;
+	
+	
+	public ClaseA() {
+		
+	}
+
+
+	public String getAtributoA() {
+		return atributoA;
+	}
+
+
+	public void setAtributoA(String atributoA) {
+		this.atributoA = atributoA;
+	}
+
+
+	public int getAtributoB() {
+		return atributoB;
+	}
+
+
+	public void setAtributoB(int atributoB) {
+		this.atributoB = atributoB;
+	}
+
+
+	public double getAtributoC() {
+		return atributoC;
+	}
+
+
+	public void setAtributoC(double atributoC) {
+		this.atributoC = atributoC;
+	}
+
+
+	public Date getFecha() {
+		return fecha;
+	}
+
+
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	
+	
+	
+}
