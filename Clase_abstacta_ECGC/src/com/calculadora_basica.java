3ed19232a02ade77c3a1e11ff603cb64e1ad4e41
GaleanaE9 <kiraiskizero@gmail.com>
2023-07-13 08:59:54 -0600

    practica interfaces

---
+++ Clase_abstacta_ECGC/src/com/calculadora_basica.java
@@ -0,0 +1,31 @@
+package com;
+
+public class calculadora_basica extends calculadora {
+
+	public calculadora_basica() {
+		
+	}
+	
+	@Override
+	public double sumar(double a, double b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+	@Override
+	public double restar(double a, double b) {
+		// TODO Auto-generated method stub
+		return a-b;
+	}
+
+	@Override
+	public double multiplicar(double a, double b) {
+		// TODO Auto-generated method stub
+		return a*b;
+	}
+
+	
+	
+	
+	
+}
